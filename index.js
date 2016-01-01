ac0b3417cb6a4285a2a6c0529315abba6d5c1c1d
Joel Kemp <mrjoelkemp@gmail.com>
2015-12-31 22:38:13 -0500

    Allow the cache to be nullified if not supplied

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ var Registry = mana.extend({
     options.githulk = 'githulk' in options ? options.githulk : null;
     options.retries = 'retries' in options ? options.retries : 3;
     options.factor = 'factor' in options ? options.factor : 2;
+    options.cache = 'cache' in options ? options.cache : null;
 
     //
     // Make sure that the given registry is a string as we can only connect to
@@ -64,6 +65,7 @@ var Registry = mana.extend({
     this.factor = options.factor;
     this.api = options.registry;
     this.proxy = options.proxy;
+    this.cache = options.cache;
 
     //
     // Pre-compile the basic authorization so we can do updates and deletes
